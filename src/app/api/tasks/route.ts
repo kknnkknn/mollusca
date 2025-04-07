e3a5910e79e7323015636df3e2d8837bd4f7fd41
naoki.kiriya <naokikiriya0314@gmail.com>
2025-04-07 17:50:43 +0900

    feat(api): Add auth check to GET tasks API

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,13 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from '@/lib/prisma'
-import { title } from "process";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET(req: NextRequest) {
-  const userId = 1 // 仮の ID
+  const session = await getServerSession(authOptions)
+  
+  if (!session?.user?.email) {
+    return NextResponse.json({ error: 'Unauthorized'}, { status: 401 })
+  }
+
+  const user = await prisma.user.findMany({
+    where: { email: session.user.email },
+  })
+
+  if (!user) {
+    return NextResponse.json({ error: 'User not found'}, { status: 404 })
+  }
   const tasks = await prisma.task.findMany({
-    where: { userId },
+    where: { userId: user.id },
     orderBy: { createdAt: 'desc' }
   })
+
   return NextResponse.json(tasks)
 }
 

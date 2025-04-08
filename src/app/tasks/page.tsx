505c9b5e017747c7d6b45f802ad736282a95ccb6
naoki.kiriya <naokikiriya0314@gmail.com>
2025-04-08 17:53:57 +0900

    feat(frontend): add task list page (data fetching from API incomplete)

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
-export default function TasksPage() {
+async function getTasks() {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/tasks`, {
+    cache: 'no-store'
+  })
+
+  if (!res.ok) {
+    throw new Error('タスク取得エラー')
+  }
+
+  return res.json()
+}
+export default async function TasksPage() {
+  const tasks = await getTasks()
   return (
-    <div>
-      <h1>Tasks ページ </h1>
-      <p>認証済みユーザのタスク一覧が表示される予定</p>
+    <div className='max-w-2x1 mx-auto mt-8'>
+      <h1 className='text-3x1 font-bold mb-6'>タスク一覧</h1>
+      <ul className='bg-white shadow-md rounded-1g overflow-hidden'>
+      {tasks.map((task: any) =>(
+        <li key={task.id} className='border-b p-4 flex justify-between items-center'>
+          <span>{task.title}</span>
+          <button className='text-red-500'>削除</button>
+        </li>
+      ))}
+      </ul>
     </div>
   )
 }
